74921d670e30d65042277b94c1d239e133c03103
jonhe <jonhe@DESKTOP-M6RGGGI>
2025-04-21 14:36:57 -0300

    updated logic

---
+++ Clive/Logic/IConfigurationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clive.Logic
+{
+    public interface IConfigurationService
+    {
+        Task<int> GetIntervalMinutesAsync();
+        Task SetIntervalMinutesAsync(int minutes);
+
+    }
+}
